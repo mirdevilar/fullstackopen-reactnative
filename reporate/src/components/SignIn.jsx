f7eed30c51599bf1e0696b44d71924c7ab5586f0
mvilark <mvilark@protonmail.com>
2024-12-13 01:17:28 +0100

    add unstyled sign in form

--- reporate/src/components/SignIn.jsx
+++ reporate/src/components/SignIn.jsx
@@ -1,7 +1,48 @@
+import { Pressable, TextInput, View } from 'react-native';
+import { useFormik } from 'formik';
+
 import Text from './Text';
 
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const SignInForm = ({ onSubmit }) => {
+  const formik = useFormik({
+    initialValues,
+    onSubmit,
+  });
+
+  return (
+    <View>
+      <TextInput
+        onChange={formik.handleChange('username')}
+        placeholder="Username"
+        value={formik.values.username}
+      />
+      <TextInput
+        onChange={formik.handleChange('password')}
+        placeholder="Password"
+        secureTextEntry
+        value={formik.values.password}
+      />
+
+      <Pressable onPress={formik.handleSubmit}>
+        <Text>Yes</Text>
+      </Pressable>
+    </View>
+  );
+};
+
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <SignInForm onSubmit={onSubmit} />
+  );
 };
 
 export default SignIn;
