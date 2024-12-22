3fdd355325b2d72e6d2ebd5da56d1847196642ec
mvilark <mvilark@protonmail.com>
2024-12-22 18:40:42 +0100

    10.12 - isolate environment data from the source code

--- reporate/App.js
+++ reporate/App.js
@@ -1,5 +1,7 @@
 import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
+import Constants from 'expo-constants';
+
 import { StatusBar } from 'expo-status-bar';
 import createApolloClient from './src/utils/apolloClient';
 
@@ -8,6 +10,8 @@ import Main from './src/components/Main';
 const apolloClient = createApolloClient();
 
 function App() {
+  console.log(Constants.expoConfig);
+
   return (
     <>
       <NativeRouter>
--- reporate/src/utils/apolloClient.js
+++ reporate/src/utils/apolloClient.js
@@ -1,8 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import Constants from 'expo-constants'
 
 const createApolloClient = () => {
   return new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    uri: Constants.expoConfig.extra.env.APOLLO_URI,
     cache: new InMemoryCache(),
   });
 };
